261684eb3564934b42e7fdae2596a8283c073594
Pekkzz <104335395+Evan-Roams@users.noreply.github.com>
2026-05-13 16:18:18 -0500

    NPC Update
    - Added npc Basic Motions package
    - Added Navmesh Surface
    - Added new npc
    - Added navmesh agent no npc
    - Configurated iddle and walking
    - Configurated paths for npcs

---
+++ Assets/Scripts/NPCPathFollower.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCPathFollower : MonoBehaviour
+{
+    public Animator anim;
+    public Transform[] startPath;
+
+    public Transform[] truePath;
+    public Transform[] falsePath;
+    private bool waitingDecision = false;
+
+    private bool? decisionResult = null;
+
+    private NavMeshAgent agent;
+
+    private int currentPoint = 0;
+
+    private Transform[] currentPath;
+
+    private bool choosingRoute = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.speed = 2f;
+
+        currentPath = startPath;
+
+        GoToNextPoint();
+    }
+
+    void Update()
+    {
+        anim.SetBool("isWalking", agent.velocity.magnitude > 0.1f);
+        if (waitingDecision)
+        return;
+
+        if (agent.pathPending)
+            return;
+
+        // Si llegó al punto
+        if (agent.remainingDistance <= 0.2f)
+        {
+            currentPoint++;
+
+            // ¿Terminó el camino actual?
+            if (currentPoint >= currentPath.Length)
+            {
+                // Si aún no decidió ruta
+                if (!choosingRoute)
+                {
+                    choosingRoute = true;
+
+                    waitingDecision = true;
+
+                    agent.isStopped = true;
+
+                    Debug.Log("Esperando decisión...");
+                }
+            }
+            else
+            {
+                GoToNextPoint();
+            }
+        }
+    }
+
+    void GoToNextPoint()
+    {
+        agent.SetDestination(currentPath[currentPoint].position);
+    }
+
+    bool MiFuncionDecision()
+    {
+        // Aquí tu lógica
+
+        // ejemplo random
+        return Random.value > 0.5f;
+    }
+
+    public void SetDecision(bool result)
+    {
+        decisionResult = result;
+
+        waitingDecision = false;
+
+        agent.isStopped = false;
+
+        if (decisionResult == true)
+        {
+            currentPath = truePath;
+        }
+        else
+        {
+            currentPath = falsePath;
+        }
+
+        currentPoint = 0;
+
+        GoToNextPoint();
+    }
+}
--- Assets/Scripts/Player/MultipleCamSystem.cs
+++ Assets/Scripts/Player/MultipleCamSystem.cs
@@ -1,16 +1,51 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MultipleCamSystem : MonoBehaviour
 {
+    public GameObject[] cameras;
+    public int currentCameraID;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentCameraID = -1;
+        nextCam();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void nextCam()
     {
-        
+        if (currentCameraID+1 > cameras.Length-1)
+        {
+            currentCameraID = 0;
+        }
+        else
+        {
+            currentCameraID = currentCameraID + 1;
+        }
+
+        foreach (GameObject cam in cameras)
+        {
+            cam.SetActive(false);
+        }
+        cameras[currentCameraID].SetActive(true);
+
+    }
+
+    public void previusCam()
+    {
+        if (currentCameraID-1 < 0)
+        {
+            currentCameraID = cameras.Length-1;
+        }
+        else
+        {
+            currentCameraID = currentCameraID - 1;
+        }
+
+        foreach (GameObject cam in cameras)
+        {
+            cam.SetActive(false);
+        }
+        cameras[currentCameraID].SetActive(true);
     }
 }
